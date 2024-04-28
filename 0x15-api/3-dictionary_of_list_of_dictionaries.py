99de484f7fd4d4f24c91171ccd3678ad37406589
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-04-28 17:41:57 +0300

    test

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,22 +7,25 @@ import sys
 
 
 if __name__ == '__main__':
-     url = "https://jsonplaceholder.typicode.com/users"
-     response = requests.get(url)
-     users = response.json()
-     dictionary = {}
-     for user in users:
-          user_id = user.get('id')
-          username = user.get('username')
-          url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id) + '/todos'
-          response = requests.get(url)
-          tasks = response.json()
-          dictionary[user_id] = []
-          for task in tasks:
-               dictionary[user_id].append({
-                    "task": task.get('title'),
-                    "completed": task.get('completed'),
-                    "username": username
-               })
-     with open('todo_all_employees.json', 'w') as filename:
-        json.dump(dictionary, filename)
+    url = "https://jsonplaceholder.typicode.com/users"
+
+    response = requests.get(url)
+    users = response.json()
+
+    dictionary = {}
+    for user in users:
+        user_id = user.get('id')
+        username = user.get('username')
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+        url = url + '/todos/'
+        response = requests.get(url)
+        tasks = response.json()
+        dictionary[user_id] = []
+        for task in tasks:
+            dictionary[user_id].append({
+                "task": task.get('title'),
+                "completed": task.get('completed'),
+                "username": username
+            })
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(dictionary, file)
